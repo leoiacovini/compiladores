b996ba402220d3bf1dbe9ad880465f010b7401ee
Leonardo Iacovini <leoiacovini@gmail.com>
2018-09-26 23:39:52 -0300

    :nail_care:

--- src/main/scala/event_machine/EventMachine.scala
+++ src/main/scala/event_machine/EventMachine.scala
@@ -1,17 +1,14 @@
 package event_machine
 
 case class Event[Input, Output, State](input: Input, output: Output, state: State)
-
 case class EventResult[Output, State](output: Output, state: State)
-
 object EventMachine {
   type ConsumeFn[Input, Output, State] = Event[Input, Output, State] => EventResult[Output, State]
 }
 
-case class EventMachine[Input, Output, State]
-(initialState: State,
- initialOutput: Output,
- consumeFn: EventMachine.ConsumeFn[Input, Output, State]) {
+case class EventMachine[Input, Output, State](initialState: State,
+                                              initialOutput: Output,
+                                              consumeFn: EventMachine.ConsumeFn[Input, Output, State]) {
 
   def consume(inputs: Seq[Input]): Output = {
     inputs.foldLeft(EventResult(initialOutput, initialState)) { (eventResult, input) =>
