122c11dcdf69ded2a48355a9a3523f339bdbbca0
Rafael Leal <rafaelealdias@gmail.com>
2018-09-26 22:51:41 -0300

    Consumers and stuff

---
+++ dartmouth-basic/src/main/scala/ConsumeFile.scala
@@ -0,0 +1,13 @@
+import scala.io.Source
+
+object ConsumeFile {
+
+  trait ConsumeFileOutput // find better name
+  case class FileLine(line: String) extends ConsumeFileOutput
+  case object EndOfFile extends ConsumeFileOutput
+
+  def apply(filepath: String): Seq[ConsumeFileOutput] = {
+    val lines: List[ConsumeFileOutput] = for(line <- Source.fromFile(filepath).getLines().toList) yield FileLine(line)
+    lines :+ EndOfFile
+  }
+}
--- dartmouth-basic/src/main/scala/event_machine/EventMachine.scala
+++ dartmouth-basic/src/main/scala/event_machine/EventMachine.scala
@@ -1,12 +1,17 @@
 package event_machine
 
+
+object EventMachine {
+  def consume[Input, Output, State](em: EventMachine[Input, Output, State], input: Input) = {
+    val (outputs, newState) = em.consumeFn(input, em.state)
+    (outputs, em.copy(state = newState))
+  }
+}
 case class EventMachine[Input, Output, State](state: State, consumeFn: (Input, State) => (Seq[Output], State)) {
   def consumeMultiple(inputs: Seq[Input]): Seq[Output] = {
-    inputs.foldLeft ((Seq.empty[Output], this)) { case ((outputs, eventmachine), input) =>
-        consumeFn(input, eventmachine.state) match {
-          case (newOutputs, newState) =>
-            (outputs ++ newOutputs, eventmachine.copy(state = newState))
-        }
+    inputs.foldLeft ((Seq.empty[Output], this)) { case ((outputs, eventMachine), input) =>
+      val (newOutputs, newEventMachine) = EventMachine.consume(eventMachine, input)
+      (outputs ++ newOutputs, newEventMachine)
     }._1
   }
 }
