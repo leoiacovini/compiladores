60ad159a9634f259939308a7fbb3c8980247b871
Leonardo Iacovini <leoiacovini@gmail.com>
2018-12-17 17:58:33 -0800

    Fix runner

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,10 +6,17 @@ import common.event_machine.EventMachine
 import common.event_machine.EventMachine.{Event, EventResult}
 import llvm.{LLVMProgram, OutputWriter}
 
+import scala.io.Source
+
 object Main extends App {
-  val lexerGrammarFilepath: String = getClass.getResource("/basic-lexer-grammar.wirth").getPath
+  if (args.length != 3) {
+    println("Required 2 argument [basic-grammar-wirth] [program-file] [outfile]")
+    throw new IllegalArgumentException()
+  }
+  val lexerGrammarFilepath: String = args(0)
   val transducer = TransducerBuilder.getTransducer(lexerGrammarFilepath)
-  val filename: String = args(0)
+  println("Transducer Created")
+  val filename: String = args(1)
   val fileConsumer = EventMachine[String, ConsumeFileOutput, Int](0, { case Event(input: String, _) =>
     val lines: Seq[ConsumeFile.ConsumeFileOutput] = ConsumeFile(input)
     lines.foreach(println)
@@ -19,13 +26,18 @@ object Main extends App {
     val chars: Seq[ConsumeLineOutput] = ConsumeLine(input)
     EventResult(chars, 0)
   })
+  println("Starting Parses...")
   val lines = fileConsumer.consume(Seq(filename))
   val chars: Seq[ConsumeLineOutput] = lineConsumer.consume(lines)
   val ascii: Seq[ConsumeLine.AsciiChar] = chars collect {case c: ConsumeLine.AsciiChar => c}
+  println("Starting Transducer...")
   val lexical = transducer.transduce(ascii)
+  println("Starting Classification...")
   val basicProgram: Seq[BasicStatement] = BasicClassifier.tokenize(lexical)
+  println("Starting LLVM Code Generation...")
   val llvm: LLVMProgram = BasicToLLVM.toLLVM(basicProgram)
 
-  OutputWriter.write(llvm, filename + ".ll")
+  OutputWriter.write(llvm, args(2) + ".ll")
+  println("DONE!")
 //  .foreach(println)
 }
